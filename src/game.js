6649c27ca6c1cb3a376950847667a8da1e957e96
Dietrich Epp <depp@zdome.net>
2015-12-12 17:13:09 -0800

    cleanup: move settle to physics.js

--- src/game.js
+++ src/game.js
@@ -19,7 +19,6 @@ var time = require('./time');
 
 var FovY = 18;
 
-
 /*
  * Calculate the interpolated position of a body.
  */
@@ -27,53 +26,6 @@ function bodyPos(body, frac) {
 	// P2 can do this for us, but we will figure that out later.
 	var p = body.position, pp = body.previousPosition;
 	return [pp[0] + (p[0] - pp[0]) * frac, pp[1] + (p[1] - pp[1]) * frac];
-}
-
-/*
- * Run the simulation until all bodies are settled.
- *
- * world: The world
- * dt: Timestep
- * maxTime: Maximum settling time
- */
-function settle(world, dt, maxTime) {
-	var oldSleep = world.sleepMode;
-	world.sleepMode = p2.World.BODY_SLEEPING;
-	var bodies = world.bodies, i, b;
-	var iter, maxIter = Math.ceil(maxTime / dt);
-	/*jshint -W018*/
-	if (!(maxIter < 1000)) {
-		maxIter = 1000;
-	}
-	if (!(maxIter > 0)) {
-		maxIter = 1;
-	}
-	var SLEEPING = p2.Body.SLEEPING, STATIC = p2.Body.STATIC;
-	var numAwake;
-	for (iter = 0; iter < maxIter; iter++) {
-		numAwake = 0;
-		for (i = 0; i < bodies.length; i++) {
-			b = bodies[i];
-			if (b.type !== STATIC && b.sleepState !== SLEEPING) {
-				numAwake++;
-			}
-		}
-		if (numAwake === 0) {
-			break;
-		}
-		world.step(dt);
-	}
-	if (iter >= maxIter) {
-		console.warn('Could not settle simulation, awake:', numAwake);
-	} else {
-		// console.log('Settling time: ', iter * dt);
-	}
-	var AWAKE = p2.Body.AWAKE;
-	for (i = 0; i < bodies.length; i++) {
-		bodies[i].sleepState = AWAKE;
-	}
-	// Doesn't work without waking all the bodies up
-	world.sleepMode = oldSleep;
 }
 
 /*
@@ -120,7 +72,7 @@ function Game() {
 	this.plane1.addShape(shape);
 	this.world.addBody(this.plane1);
 
-	settle(this.world, 1 / param.Rate, 3.0);
+	physics.settle(this.world, 1 / param.Rate, 3.0);
 
 	// Camera
 	this.camera = new camera.Camera({
--- src/physics.js
+++ src/physics.js
@@ -33,7 +33,55 @@ function createWorld() {
 	return world;
 }
 
+/*
+ * Run the simulation until all bodies are settled.
+ *
+ * world: The world
+ * dt: Timestep
+ * maxTime: Maximum settling time
+ */
+function settle(world, dt, maxTime) {
+	var oldSleep = world.sleepMode;
+	world.sleepMode = p2.World.BODY_SLEEPING;
+	var bodies = world.bodies, i, b;
+	var iter, maxIter = Math.ceil(maxTime / dt);
+	/*jshint -W018*/
+	if (!(maxIter < 1000)) {
+		maxIter = 1000;
+	}
+	if (!(maxIter > 0)) {
+		maxIter = 1;
+	}
+	var SLEEPING = p2.Body.SLEEPING, STATIC = p2.Body.STATIC;
+	var numAwake;
+	for (iter = 0; iter < maxIter; iter++) {
+		numAwake = 0;
+		for (i = 0; i < bodies.length; i++) {
+			b = bodies[i];
+			if (b.type !== STATIC && b.sleepState !== SLEEPING) {
+				numAwake++;
+			}
+		}
+		if (numAwake === 0) {
+			break;
+		}
+		world.step(dt);
+	}
+	if (iter >= maxIter) {
+		console.warn('Could not settle simulation, awake:', numAwake);
+	} else {
+		// console.log('Settling time: ', iter * dt);
+	}
+	var AWAKE = p2.Body.AWAKE;
+	for (i = 0; i < bodies.length; i++) {
+		bodies[i].sleepState = AWAKE;
+	}
+	// Doesn't work without waking all the bodies up
+	world.sleepMode = oldSleep;
+}
+
 module.exports = {
 	Material: Material,
 	createWorld: createWorld,
+	settle: settle,
 };
