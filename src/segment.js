43e172dd19afe2aeff2a1e5b746027a8c4829269
Dietrich Epp <depp@zdome.net>
2015-12-12 22:21:43 -0800

    tweak border generation

--- src/segment.js
+++ src/segment.js
@@ -19,25 +19,36 @@ var util = require('./util');
  * posY: The edge of the tile, vertically
  */
 function emitBorder(tiles, minX, maxX, dirY, posY) {
+	var remW = Math.floor((1 + maxX - minX) * 0.5);
+	var state = Math.random() < 0.5 || remW <= 5;
+	var x = minX;
 	var newTiles = [];
-	var x = minX, remW = Math.floor((1 + maxX - minX) * 0.5);
 	while (remW > 0) {
-		var tw = util.randInt(2, 10);
-		remW -= tw;
-		if (remW <= 1) {
-			if (tw <= 4 || remW <= 0) {
-				tw += remW;
-				remW = 0;
+		var tw, th;
+		if (state) {
+			tw = util.randInt(3, 5);
+			th = 3;
+			state = false;
+		} else {
+			tw = util.randInt(5, 10);
+			th = 2;
+			state = true;
+		}
+		if (tw >= remW) {
+			tw = remW;
+		} else if (tw >= remW - 2) {
+			if (tw < 5) {
+				tw = remW;
 			} else {
 				tw -= 2;
-				remW += 2;
 			}
 		}
+		remW -= tw;
 		newTiles.push({
 			x: x + tw,
-			y: posY + dirY,
+			y: posY + th * dirY,
 			w: tw * 2,
-			h: 2,
+			h: th * 2,
 			color: color.rgb(Math.random(), Math.random(), Math.random()),
 		});
 		x += tw * 2;
@@ -57,10 +68,10 @@ function Segment() {
  */
 Segment.prototype.activate = function(game) {
 	game.tiles.clear();
-	var height = param.LevelY;
+	var height = param.LevelY * 0.5;
 	var floor = height * -0.5, ceiling = height * 0.5;
-	emitBorder(game.tiles, -32, +32, -1, floor);
-	emitBorder(game.tiles, -32, +32, +1, ceiling);
+	emitBorder(game.tiles, -32, +128, -1, floor);
+	emitBorder(game.tiles, -32, +128, +1, ceiling);
 };
 
 module.exports = {
